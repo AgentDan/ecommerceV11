63736961c1ba65f481793c142e40b62fe1df8a06
AgentSandbox <pregrada@mail.com>
2024-03-27 00:59:11 +0100

    com1

---
+++ client/src/Pages/Main/ProjectOne/ModelOne.js
@@ -0,0 +1,61 @@
+import React, {useRef} from 'react'
+import {useGLTF, useScroll} from "@react-three/drei"
+import {useFrame} from "@react-three/fiber"
+import {gsap} from "gsap"
+import {useLayoutEffect} from "react"
+
+const ModelOne = () => {
+    const {nodes, materials} = useGLTF('./models/lockerGSAP.glb')
+    const mod = useRef()
+    const tl = useRef()
+    const scroll = useScroll()
+    const G1 = useRef()
+    const G2 = useRef()
+    const G3 = useRef()
+
+    useFrame ((state , delta) => {
+        tl.current.seek(scroll.offset * tl.current.duration())
+    })
+
+    useLayoutEffect(()=> {
+        tl.current = gsap.timeline({defaults: {duration: 2, ease: 'power1.inOut'}})
+        mod.current.rotation.y = 5
+        mod.current.position.y = -0.3
+        mod.current.position.z = -0.5
+        tl.current
+            .to(G1.current.rotation, {y: -0.8}, 0)
+            .to(G2.current.rotation, {y: -1.8}, 0)
+            .to(G1.current.position, {y: 8}, 3)
+            .to(G2.current.position, {y: 9}, 3)
+            .to(G2.current.rotation, {y: 0}, 4)
+            .to(G2.current.position, {y: 17}, 7)
+            .to(G3.current.position, {y: 19.5}, 7)
+            .to(G3.current.rotation, {y: -0.8}, 8)
+    },[])
+
+    return (
+        <>
+            <group dispose={null} ref={mod}>
+
+                <group ref={G1}>
+                    <mesh geometry={nodes.G11.geometry} material={materials.G11} />
+                    <mesh geometry={nodes.G12.geometry} material={materials.G12} />
+                </group>
+
+                <group ref={G2}>
+                    <mesh geometry={nodes.G21.geometry} material={materials.G21} />
+                    <mesh geometry={nodes.G22.geometry} material={materials.G22} />
+                </group>
+
+                <group ref={G3}>
+                    <mesh geometry={nodes.G31.geometry} material={materials.G31} />
+                    <mesh geometry={nodes.G32.geometry} material={materials.G32} />
+                </group>
+
+            </group>
+
+        </>
+    )
+};
+
+export default ModelOne
---
+++ client/src/Pages/Main/ProjectOne/ProjectOne.js
@@ -0,0 +1,91 @@
+import {Environment, OrbitControls, Scroll, ScrollControls} from "@react-three/drei"
+import './ProjectOne.css'
+import ModelOne from "./ModelOne.js"
+
+function ProjectOne () {
+    return (
+        <>
+            <Environment preset='warehouse'/>
+
+            <ScrollControls pages={5} damping={0.2}>
+
+                <ModelOne/>
+                <color attach="background" args={["#999999"]}/>
+
+                <Scroll html style={{width: '100%'}}>
+                    <h1 style={{
+                        color: '#87CEEB',
+                        position: 'absolute',
+                        top: `65vh`,
+                        left: '50%',
+                        fontSize: '4em',
+                        transform: `translate(-50%,-50%)`
+                    }}>
+                        Belgrade
+                    </h1>
+                    <h1 style={{
+                        color: '#87CEEB',
+                        position: 'absolute',
+                        top: `75vh`,
+                        left: '50%',
+                        fontSize: '4em',
+                        transform: `translate(-50%,-50%)`
+                    }}>
+                        Woterfront
+                    </h1>
+
+                    <div style={{position: 'absolute', top: `132vh`}}>
+                        <div>
+                            <h1>ELITE</h1>
+                        </div>
+                        <div className='row'>
+                            <p style={{maxWidth: '400px'}}>
+                                A luxury cabinet epitomizes the epitome of opulence
+                                and refinement in furniture design. Crafted with the
+                                utmost attention to detail and using the finest materials,
+                                it stands as a statement piece in any room, exuding elegance
+                                and sophistication.</p>
+
+                        </div>
+
+                    </div>
+
+                    <div className='row' style={{top: `264vh`}}>
+                        <div>
+                            <h1>MIDDLE</h1>
+                        </div>
+                        <div className='row'>
+                            <p style={{maxWidth: '400px'}}>A middle-class cabinet offers practicality and functionality
+                                without compromising on style. Crafted from durable materials such as engineered wood or
+                                solid hardwood veneers, it balances affordability with quality construction.
+
+                                Typically featuring a straightforward design, a middle-class cabinet focuses on
+                                practical storage solutions. It may include adjustable shelves, drawers with smooth
+                                gliding mechanisms, and simple hardware for ease of use. While it may not boast
+                                intricate carvings or luxurious finishes, it exudes a sense of warmth and homeliness
+                                that complements a variety of interior styles.</p>
+                        </div>
+
+                    </div>
+
+                    <div className='row' style={{top: `396vh`}}>
+                        <div>
+                            <h1>ECONOMY</h1>
+                        </div>
+                        <div className='row'>
+                            <p style={{maxWidth: '400px'}}>The economy class cabinet is primarily designed for those
+                                seeking a simple and budget-friendly storage solution for clothing and other items
+                                without unnecessary frills and expensive additions.</p>
+                        </div>
+
+                    </div>
+
+                    {/*<h1 style={{position: 'absolute', top: '528vh'}}>HELLO4</h1>*/}
+
+                </Scroll>
+            </ScrollControls>
+        </>
+    )
+}
+
+export default ProjectOne
